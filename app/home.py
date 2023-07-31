607b2da4e3fb97ac400a707d9bc86fd83f8c1d1d
Rodolpho Braga <rodolpho@insilicall.com>
2023-07-31 10:50:39 -0300

    fix image logo

--- app/home.py
+++ app/home.py
@@ -1,10 +1,17 @@
+import os
 import streamlit as st
 from PIL import Image
 
-def app(_,s_state):
-    #s_state.title = 'Home'
-    st.image('LOGO-QSAR-lit.tif')
-    
+def app(_, s_state):
+    # Get the current file's directory
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+
+    # Construct the path to the image file
+    image_path = os.path.join(current_dir, 'LOGO-QSAR-lit.tif')
+
+    # Display the image
+    st.image(image_path)
+
     st.markdown("""
     <style>
     p {
@@ -18,6 +25,4 @@ def app(_,s_state):
     **Credits**
     - App built in `Python` + `Streamlit` by José Teófilo Moreira Filho (<a href="http://lattes.cnpq.br/3464351249761623" target="_blank">Lattes</a>, <a href="https://scholar.google.com/citations?user=0I1GiOsAAAAJ&hl=pt-BR" target="_blank">Google Scholar</a> ,<a href="https://orcid.org/0000-0002-0777-280X" target="_blank">ORCID</a>), <a href="https://rcbraga.github.io/" target="_blank">Rodolpho C. Braga</a>, Henric P. V. Gil, Vinicius M. Alves , Bruno J. Neves, and <a href="http://labmol.com.br/" target="_blank">Carolina H. Andrade</a>.
     </p>
-    """,unsafe_allow_html=True)
-    #Page description and credits
-    #image = Image.open('logo.png')
+    """, unsafe_allow_html=True)
